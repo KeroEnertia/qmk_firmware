ed267fd678789616e979bad7a8cfbb1ec10522b5
KeroEnertia <keroenertia@gmail.com>
2021-06-04 01:03:17 -0500

    enabled bootmagic on runic

--- keyboards/planck/keymaps/runic/rules.mk
+++ keyboards/planck/keymaps/runic/rules.mk
@@ -1,7 +1,7 @@
 SRC += muse.c
 
 DIP_SWITCH_ENABLE	=	no	# Physical DIP switch configuration
-BOOTMAGIC_ENABLE	=	no	# Virtual DIP switch configuration(+1000)
+BOOTMAGIC_ENABLE	=	yes	# Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE		=	no	# Mouse keys(+4700)
 EXTRAKEY_ENABLE		=	yes	# Audio control and System control(+450)
 CONSOLE_ENABLE		=	yes	# Console for debug(+400)
